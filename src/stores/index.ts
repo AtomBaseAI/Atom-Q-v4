500de89829b2ae4efba2c0fac93555df44af44cc
AtomLabs <atombaseinc@gmail.com>
2026-02-07 16:21:41 +0530

    x

--- src/stores/index.ts
+++
@@ -1,4 +0,0 @@
-export { useUserStore } from './user'
-export { useQuizCacheStore } from './quiz-cache'
-export { useSettingsStore } from './settings'
-export { useQuizProgressStore } from './quiz-progress'
--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-
-export * from './auth'
-export * from './quiz'
-export * from './question'
-export * from './user'
-export * from './api'
